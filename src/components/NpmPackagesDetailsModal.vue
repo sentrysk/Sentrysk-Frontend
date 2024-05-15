0f049a0e5739ed2d46758d0450cd9a38ece95745
keraattin <kerem-oruc@outlook.com>
2024-05-15 13:29:23 +0300

    Npm package details modal added

---
+++ src/components/NpmPackagesDetailsModal.vue
@@ -0,0 +1,54 @@
+<template>
+    <!-- Modal -->
+    <div class="modal fade" id="npmPackageDetailsModal" data-bs-keyboard="false" tabindex="-1" aria-labelledby="npmPackageDetailsModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="npmPackageDetailsModalLabel">{{ npmPackageName }}</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <table class="table table-sm table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Version</th>
+                                <th>Agents</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="(agents, version) in versions" :key="version">
+                                <td>{{ version }}</td>
+                                <td>
+                                    <tr v-for="agent in agents">
+                                        <a href="#" @click.prevent="goToDetails(agent)" data-bs-dismiss="modal">{{ agent }}</a>
+                                    </tr>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+  <script>
+  export default {
+    name: 'NpmPackagesDetailsModal',
+    props: ['npmPackageName', 'versions'],
+    methods: {
+      goToDetails(id) {
+        // Navigate to the dynamic route /agents/:id
+        this.$router.push({ name: 'agentData', params: { id: id } });
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Styles for modal */
+  </style>
+  
