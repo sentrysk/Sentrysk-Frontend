c83094cfba3ebbe562fd924a562dd81f0cab545e
keraattin <kerem-oruc@outlook.com>
2024-05-15 12:42:34 +0300

    Npm packages nav added to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -30,6 +30,9 @@
             <li class="nav-item">
               <router-link class="nav-link" to="/pippackages"><i class="fa-brands fa-python"></i> Pip Packages</router-link>
             </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/npmpackages"><i class="fa-brands fa-npm"></i> Npm Packages</router-link>
+            </li>
           </ul>
           <div class="dropdown d-flex">
             <button
