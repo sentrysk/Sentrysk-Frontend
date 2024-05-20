a7241f9cdbdbac2e9870b31aa237e260b0a33e39
keraattin <kerem-oruc@outlook.com>
2024-05-20 17:15:11 +0300

    #3 My Profile Page Initiated

---
+++ src/views/MyProfileView.vue
@@ -0,0 +1,46 @@
+<template>
+    <Navbar /> <!-- Include the Navbar component here -->
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+
+            </div>
+        </div>
+    </div>
+    
+</template>
+  
+<script>
+  import Navbar from '@/components/Navbar.vue';
+  import { getLoggedUserInfo } from '@/utils/requestUtils';
+  
+  export default {
+    name: 'DashboardLastLoginsView',
+    components: {
+      Navbar,
+    },
+    data(){
+        return{
+            name:"",
+            lastname:"",
+            email:"",
+            created:""
+        }
+    },
+    mounted(){
+        this.fillLoggedUserInfo();
+    },
+    methods:{
+        async fillLoggedUserInfo(){
+            const loggedUserData = await getLoggedUserInfo();
+        }
+    }
+  }
+</script>
+
+<style>
+  #dashboardLastLoginsTable{
+    margin-top: 5rem;
+  }
+</style>
+  
