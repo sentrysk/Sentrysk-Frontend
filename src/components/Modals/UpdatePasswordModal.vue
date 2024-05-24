ad9669f4c5019878bd7cfe59fda5aed237940348
keraattin <kerem-oruc@outlook.com>
2024-05-24 15:31:54 +0300

    #4 Input fields updated

--- src/components/Modals/UpdatePasswordModal.vue
+++ src/components/Modals/UpdatePasswordModal.vue
@@ -1,58 +1,65 @@
 <template>
     <!-- Modal -->
     <div class="modal fade" id="updatePasswordModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="updatePasswordModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-lg">
+            <form @submit.prevent="updatePassword">
             <div class="modal-content">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="updatePasswordModalLabel">Update Password</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <div class="mb-4 input-group">
-                        <span class="input-group-text"><i class="bi bi-lock"></i></span>
-                        <input
-                        :type="showPassword ? 'text' : 'Current Password'"
-                        v-model="current_password"
-                        class="form-control"
-                        placeholder="Current Password"
-                        required
-                        />
-                        <button type="button" class="btn btn-outline-secondary" @click="togglePasswordVisibility">
-                        <i :class="['bi', showPassword ? 'bi-eye-slash' : 'bi-eye']"></i>
-                        </button>
-                    </div>
-                    <div class="mb-4 input-group">
-                        <span class="input-group-text"><i class="bi bi-lock"></i></span>
-                        <input
-                        :type="showPassword ? 'text' : 'New Password'"
-                        v-model="new_password"
-                        class="form-control"
-                        placeholder="New Password"
-                        required
-                        />
-                        <button type="button" class="btn btn-outline-secondary" @click="togglePasswordVisibility">
-                        <i :class="['bi', showPassword ? 'bi-eye-slash' : 'bi-eye']"></i>
-                        </button>
-                    </div>
-                    <div class="mb-4 input-group">
-                        <span class="input-group-text"><i class="bi bi-lock"></i></span>
-                        <input
-                        :type="showPassword ? 'text' : 'New Password Repeat'"
-                        v-model="new_password_repeat"
-                        class="form-control"
-                        placeholder="New Password Repeat"
-                        required
-                        />
-                        <button type="button" class="btn btn-outline-secondary" @click="togglePasswordVisibility">
-                        <i :class="['bi', showPassword ? 'bi-eye-slash' : 'bi-eye']"></i>
-                        </button>
+                    <div class="card mt-5 shadow">
+                        <div class="card-body">
+                            <div class="mb-4 input-group">
+                                <span class="input-group-text"><i class="bi bi-lock"></i></span>
+                                <input
+                                    :type="showCurrentPassword ? 'text' : 'password'"
+                                    v-model="currentPassword"
+                                    class="form-control"
+                                    placeholder="Password"
+                                    required
+                                />
+                                <button type="button" class="btn btn-outline-secondary" @click="toggleCurrentPasswordVisibility">
+                                    <i :class="['bi', showCurrentPassword ? 'bi-eye-slash' : 'bi-eye']"></i>
+                                </button>
+                            </div>
+                            <div class="mb-4 input-group">
+                                <span class="input-group-text"><i class="bi bi-lock"></i></span>
+                                <input
+                                    :type="showNewPassword ? 'text' : 'password'"
+                                    v-model="newPassword"
+                                    class="form-control"
+                                    placeholder="Password"
+                                    required
+                                />
+                                <button type="button" class="btn btn-outline-secondary" @click="toggleNewPasswordVisibility">
+                                    <i :class="['bi', showNewPassword ? 'bi-eye-slash' : 'bi-eye']"></i>
+                                </button>
+                            </div>
+                            <div class="mb-4 input-group">
+                                <span class="input-group-text"><i class="bi bi-lock"></i></span>
+                                <input
+                                    :type="showNewPasswordRepeat ? 'text' : 'password'"
+                                    v-model="newPasswordRepeat"
+                                    class="form-control"
+                                    placeholder="Password"
+                                    required
+                                />
+                                <button type="button" class="btn btn-outline-secondary" @click="toggleNewPasswordRepeatVisibility">
+                                    <i :class="['bi', showNewPasswordRepeat ? 'bi-eye-slash' : 'bi-eye']"></i>
+                                </button>
+                            </div>
+                            
+                        </div>
                     </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button class="btn btn-primary is-success" @click="updatePassword">Update</button>
+                    <button class="btn btn-primary" type="submit">Update</button>
                 </div>
             </div>
+            </form>
         </div>
     </div>
 </template>
@@ -64,10 +71,29 @@ import axios from 'axios';
 
 export default {
   name: 'UpdatePasswordModal',
+  data() {
+      return {
+        currentPassword: "",
+        newPassword: "",
+        newPasswordRepeat: "",
+        showCurrentPassword: false,
+        showNewPassword: false,
+        showNewPasswordRepeat: false,
+      };
+    },
   methods:  {
     async updatePassword(){
-
-    }
+        console.log(this.currentPassword,this.newPassword,this.newPasswordRepeat)
+    },
+    toggleCurrentPasswordVisibility() {
+        this.showCurrentPassword = !this.showCurrentPassword;
+    },
+    toggleNewPasswordVisibility() {
+        this.showNewPassword = !this.showNewPassword;
+    },
+    toggleNewPasswordRepeatVisibility() {
+        this.showNewPasswordRepeat = !this.showNewPasswordRepeat;
+    },
   }
 };
 </script>
