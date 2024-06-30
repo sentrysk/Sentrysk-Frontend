ca0158a3682fa74359581027aee618fc26672f95
keraattin <kerem-oruc@outlook.com>
2024-06-30 14:33:17 +0300

    #10 getAllAgents request moved to request utils

--- src/utils/requestUtils.js
+++ src/utils/requestUtils.js
@@ -495,3 +495,19 @@ export async function getAllUsers() {
     console.error('Error fetching all users data:', error);
     }
 }
+
+export async function getAllAgents() {
+    try {
+        // Retrieve JWT token from session storage
+        const jwtToken = sessionStorage.getItem('jwtToken');
+        const URL = API_URL + AGNT_EP;
+        const response = await axios.get(URL, {
+            headers: {
+                Authorization: jwtToken,
+            },
+        });
+        return response.data;
+    } catch (error) {
+    console.error('Error fetching all agents data:', error);
+    }
+}
