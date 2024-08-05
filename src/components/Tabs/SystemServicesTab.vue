b1c7f012bf6c403fe2a2a46819189516bd218dda
keraattin <kerem-oruc@outlook.com>
2024-08-05 17:54:12 +0300

    #23 Service status field grouping added

--- src/components/Tabs/SystemServicesTab.vue
+++ src/components/Tabs/SystemServicesTab.vue
@@ -225,6 +225,44 @@
                   [25, 50, 100, 250, -1],
                   [25, 50, 100, 250, 'All']
               ],
+              columnDefs: [
+                {
+                  targets: 0,
+                  orderable: false
+                },
+              ],
+              //Column Filtering
+              initComplete: function () {
+                let columnsToFilter = [0]; // Array of column indices to add the select filter
+                this.api()
+                    .columns()
+                    .every(function (index) {
+                      if (columnsToFilter.includes(index)) {
+                        let column = this;
+        
+                        // Create select element
+                        let select = document.createElement('select');
+                        select.add(new Option(''));
+                        column.header().replaceChildren(select);
+        
+                        // Apply listener for user change in value
+                        select.addEventListener('change', function () {
+                            column
+                                .search(select.value, {exact: true})
+                                .draw();
+                        });
+        
+                        // Add list of options
+                        column
+                            .data()
+                            .unique()
+                            .sort()
+                            .each(function (d, j) {
+                                select.add(new Option(d));
+                            });
+                      }
+                    });
+              },
               });
               // Style length Menu
               const pageEntrySize = document.getElementById('systemServicesTable_length')
