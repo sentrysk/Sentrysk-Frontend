06d9cc688c67fc13df0d66352d9f4ca5be93a9ff
keraattin <kerem-oruc@outlook.com>
2024-09-12 18:57:44 +0300

    #32 accordion component added to disks

--- src/components/Tabs/SystemInfoSubTabs/LatestDiskUsageChart.vue
+++ src/components/Tabs/SystemInfoSubTabs/LatestDiskUsageChart.vue
@@ -1,27 +1,42 @@
 <template>
+  <!-- Container Block -->
   <div v-if="loading" class="chart-container col-sm-2">
-    <!-- Loading -->
     <h3 class="text-center">Disk</h3>
+    <!-- Loading State -->
     <div class="loading-content">
       <i class="fas fa-spinner fa-spin fa-3x"></i>
       <h2>Loading...</h2>
     </div>
   </div>
-  <div v-else-if="error" class="chart-container col-sm-2">
-    <!-- Error -->
-    <h3 class="text-center">Disk</h3>
+  <!-- Loaded State -->
+  <!-- If any error occurs -->
+  <div v-if="error" class="chart-container col-sm-2">
     <div class="error-content">
       <i class="fas fa-exclamation-triangle fa-3x"></i>
       <h2>Error loading data.</h2>
     </div>
   </div>
-  <div v-else v-for="(diskData, device) in disks" :key="device" class="chart-container col-sm-2">
-     <!-- Working Charts -->
-    <h3 class="text-center">{{ diskData.device }}</h3>
-    <canvas :id="'chart-' + device"></canvas>
-    <span :id="'lastUpdate-' + device"></span>
+  <!-- If any error not occurs -->
+  <div v-else class="row">
+    <div class="accordion" id="accordionDisksLatestUsage">
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseDisks" aria-expanded="true" aria-controls="collapseDisks">
+            <i class="bi bi-hdd-stack"></i> Disks
+          </button>
+        </h2>
+        <div id="collapseDisks" class="accordion-collapse collapse show" data-bs-parent="#accordionDisksLatestUsage">
+          <div class="accordion-body">
+            <div id="charts-container" class="row">
+              <!-- Content Here -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  </template>
+
+</template>
     
     <script>
     import Chart from 'chart.js/auto';
@@ -47,6 +62,10 @@
             // Get the ID from the URL
             const agentId = this.$route.params.id;
             this.disks = await getLatestDiskUsageData(agentId);
+
+            // Get Container Element
+            const container = document.getElementById('charts-container');
+            container.innerHTML = ''; // Remove previous elements in charts-container
             // Render
             this.renderCharts();
   
@@ -63,11 +82,18 @@
             Object.keys(this.disks).forEach(device => {
                 const diskData = this.disks[device];
                 const usagePercent = diskData.percent;
-                const lastUpdateField = document.getElementById('lastUpdate-' + device);
-                lastUpdateField.textContent = calculateDatetimeDifference(diskData.timestamp)
-                lastUpdateField.title = formatToLocalTime(diskData.timestamp);
-                
-                const ctx = document.getElementById('chart-' + device).getContext('2d');
+                //const lastUpdateField = document.getElementById('lastUpdate-' + device);
+                //lastUpdateField.textContent = calculateDatetimeDifference(diskData.timestamp)
+                //lastUpdateField.title = formatToLocalTime(diskData.timestamp);
+
+                // Create Header Element
+                const header = document.createElement('h5');
+                header.appendChild(document.createTextNode(diskData.device));
+
+                // Create Canvas Element
+                const canvas = document.createElement('canvas');
+                // Create Chart
+                const ctx = canvas.getContext('2d');
                 new Chart(ctx, {
                 type: 'doughnut',
                     data: {
@@ -86,6 +112,25 @@
                         }]
                     },
                 });
+
+                // Create Last Update Span
+                const lastUpdateSpan = document.createElement('span');
+                lastUpdateSpan.title = formatToLocalTime(diskData.timestamp);
+                lastUpdateSpan.textContent = "Last Update: " + calculateDatetimeDifference(diskData.timestamp);
+
+                // Create Chart Div
+                const chartDiv = document.createElement('div');
+                chartDiv.classList.add('chart-container','col-sm-2');
+
+                chartDiv.appendChild(header);
+                chartDiv.appendChild(canvas);
+                chartDiv.appendChild(lastUpdateSpan);
+
+                // Get Container Element
+                const container = document.getElementById('charts-container');
+                
+                // Apend Chart Div to Container Div
+                container.appendChild(chartDiv);
             });
         }
       },
